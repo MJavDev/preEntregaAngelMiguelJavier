844c8e9c4e183aee8462fd144bf173fca4c30464
Miguel Javier Dev <125704220+MJavDev@users.noreply.github.com>
2024-07-12 14:15:29 -0300

    Actualizar utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);// nos da la ruta desde donde se esta haciendo el import
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default __dirname;
